0e70d432aee5edbc1ba2f35a2fe29eae9ab37c76
Magno <magnocostamaia@gmail.com>
2020-01-20 01:24:30 -0300

    Minor changes

--- server.py
+++ server.py
@@ -1,12 +1,6 @@
 import socket,time,sys
 from datetime import datetime
-def acesso(autorizado):
-	with open('log_acesso.txt','a') as obj:
-		obj.write(';'.join([str(autorizado),str(datetime.now()),'\n']))
-def recusado(usuario):
-	with open('log_tentativa_nao_autorizada.txt','a') as obj:
-		obj.write(';'.join([str(usuario),str(datetime.now()),'\n']))	
-acesso_permitido = ['9B,13,24,1B','61,3F,4F,D3']
+
 while True:
 	with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
 		s.bind(("192.168.43.10",8000))
