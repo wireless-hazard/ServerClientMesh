438b5641cde1c3282d7071d446cb82712f80c57e
Magno <magnocostamaia@gmail.com>
2020-01-26 19:37:38 -0300

    It shows now the parent-child connection

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ grafico.set_xticklabels([])
 while True:
 	with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
 		s.bind(("192.168.43.10",8000))
+		# s.bind(("192.168.0.3",8000))
 		s.listen()
 		conn,addr = s.accept()
 		with conn:
@@ -37,6 +38,14 @@ while True:
 				# grafico.scatter(posicao[-1],int(layer[-1]),color='w')
 				grafico.scatter(posicao[-1],int(layer[-1]),color='c')
 				grp_text = grafico.annotate(rssi[-1]+'\n'+esp[-1],(posicao[-1],int(layer[-1])),fontsize=8)
+
+				if parent[-1] in esp:
+					plt.plot([posicao[-1],posicao[esp.index(parent[-1])]],[int(layer[-1]),int(layer[esp.index(parent[-1])])],color='c')
+				if layer[-1]=='2':
+					grafico.scatter(0,0,color='c')
+					grafico.annotate(parent[-1],(0,0),fontsize=8)
+					plt.plot([0,posicao[-1]],[0,int(layer[-1])],color='c')
+
 				fig.canvas.draw_idle()#REDESENHA O GRAFICO
 				plt.pause(0.1)
 				grp_text.remove()
